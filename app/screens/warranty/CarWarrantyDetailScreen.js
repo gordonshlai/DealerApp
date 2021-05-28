3a83641fab00da72ea1bb14567c5dd16473c6042
Gordon Lai <gordonshlai@yahoo.com.hk>
2021-05-28 14:05:05 +0100

    Added CarWarrantyDetailScreen.

--- app/screens/warranty/CarWarrantyDetailScreen.js
+++ app/screens/warranty/CarWarrantyDetailScreen.js
@@ -1,6 +1,4 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import React, { useContext, useState } from "react";
-import { TouchableOpacity } from "react-native";
+import React, { useContext } from "react";
 import {
   View,
   StyleSheet,
@@ -8,186 +6,161 @@ import {
   ScrollView,
 } from "react-native";
 import * as Yup from "yup";
-import { Formik } from "formik";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import dayjs from "dayjs";
 
 import AppButton from "../../components/AppButton";
 import AppText from "../../components/AppText";
 import Background from "../../components/Background";
 import {
-  AppErrorMessage,
   AppForm,
+  AppFormDateTimePicker,
   AppFormField,
   AppFormPicker,
   RegistrationPlateInput,
   SubmitButton,
 } from "../../components/forms";
 import Screen from "../../components/Screen";
+import ProgressBar from "./components/ProgressBar";
+import ActivityIndicator from "../../components/ActivityIndicator";
 
 import colors from "../../config/colors";
 import defaultStyles from "../../config/styles";
-import useApi from "../../hooks/useApi";
-import client from "../../api/client";
-import ActivityIndicator from "../../components/ActivityIndicator";
 import routes from "../../navigation/routes";
 import WarrantyContext from "../../warranty/context";
 
-const withRegValidationSchema = Yup.object().shape({
-  registration: Yup.string().label("Registration"),
-  mileage: Yup.number().label("Mileage"),
+const validationSchema = Yup.object().shape({
+  purchase_date: Yup.date()
+    .required()
+    .max(dayjs().add(2, "year"))
+    .label("Purchase Date"),
+  start_date: Yup.date()
+    .required()
+    .min(dayjs().subtract(5, "day"))
+    .max(dayjs().add(2, "year"))
+    .label("Warranty Start Date"),
+  mot_date: Yup.date()
+    .required()
+    .min(dayjs().subtract(1, "day"))
+    .max(dayjs().add(4, "year"))
+    .label("MOT Due Date"),
+  service_date: Yup.date()
+    .required()
+    .min(dayjs().subtract(5, "year"))
+    .max(dayjs().add(1, "day"))
+    .label("Last Service Date"),
+  service_history: Yup.string().required().label("Service History"),
+  registration: Yup.string().required().label("Registration"),
+  vin_number: Yup.string().label("VIN Number"),
 });
 
-const withoutRegValidationSchema = Yup.object().shape({
-  make: Yup.string().label("Make"),
-  model: Yup.string().label("Model"),
-});
+const serviceHistoryOptions = [
+  "Full (all with main dealer)",
+  "Full (some with main dealer)",
+  "Full (none at main dealer)",
+  "Last few services only",
+  "Last service only",
+];
 
-function CarWarrantyDetailScreen({ navigation }) {
-  const { booking } = useContext(WarrantyContext);
+function CarWarrantyDetailScreen({ route, navigation }) {
+  const {
+    vehicle: { registration },
+    booking,
+    setBooking,
+  } = useContext(WarrantyContext);
   const tabBarHeight = useBottomTabBarHeight();
 
-  const [error, setError] = useState();
-  const [withReg, setWithReg] = useState(true);
+  const handleSubmit = (values) => {
+    const payload = { ...booking, ...values };
 
-  const makesApi = useApi(() => client.get("api/car/makes"));
-  const modelsApi = useApi((endpoint) => client.get(endpoint));
+    payload.service_history = (
+      serviceHistoryOptions.indexOf(payload.service_history) + 1
+    ).toString();
+    payload.purchase_date = dayjs(payload.purchase_date).format("YYYY-MM-DD");
+    payload.start_date = dayjs(payload.start_date).format("YYYY-MM-DD");
+    payload.mot_date = dayjs(payload.mot_date).format("YYYY-MM-DD");
+    payload.service_date = dayjs(payload.service_date).format("YYYY-MM-DD");
 
-  const handleWithRegSubmit = async ({ registration, mileage }) => {
-    console.log(registration + mileage);
+    setBooking(payload);
+
     navigation.navigate(routes.CAR_WARRANTY_CUSTOMER_DETAIL);
-  };
-
-  const handleWithoutRegSubmit = async ({ make, model }) => {
-    console.log(make + model);
-    navigation.navigate(routes.CAR_WARRANTY_CUSTOMER_DETAIL);
-  };
-
-  const WithRegButton = ({ text, onPress, icon, selected }) => {
-    return (
-      <TouchableOpacity
-        style={[styles.withRegButton, selected && styles.withRegButtonSelected]}
-        onPress={onPress}
-      >
-        <MaterialCommunityIcons
-          name={icon}
-          size={40}
-          color={selected ? colors.primary : colors.mediumGrey}
-        />
-        <AppText
-          style={[styles.withRegText, selected && styles.withRegTextSelected]}
-        >
-          {text}
-        </AppText>
-      </TouchableOpacity>
-    );
-  };
-
-  const WithRegForm = () => {
-    return (
-      <AppForm
-        initialValues={{ registration: "", mileage: "" }}
-        onSubmit={handleWithRegSubmit}
-        validationSchema={withRegValidationSchema}
-      >
-        <View style={styles.fieldContainer}>
-          <RegistrationPlateInput
-            name="registration"
-            placeholder="ENTER REGISTRATION"
-            onContentSizeChange={() => setError("")}
-          />
-        </View>
-        <View style={styles.fieldContainer}>
-          <AppText style={styles.fieldTitle}>Mileage</AppText>
-          <AppFormField
-            name="mileage"
-            placeholder="Mileage"
-            keyboardType="numeric"
-            style={styles.appFormField}
-          />
-        </View>
-        <AppErrorMessage error={error} visible={error} />
-        <SubmitButton title="Next" />
-      </AppForm>
-    );
-  };
-
-  const WithoutRegForm = () => {
-    return (
-      <Formik
-        initialValues={{ make: "", model: "" }}
-        onSubmit={handleWithoutRegSubmit}
-        validationSchema={withoutRegValidationSchema}
-      >
-        {({ values, setFieldValue }) => (
-          <>
-            <View style={styles.fieldContainer}>
-              <AppText style={styles.fieldTitle}>Make</AppText>
-              <AppFormPicker
-                name="make"
-                placeholder="Please select"
-                items={makesApi.data}
-                onSelectItem={(item) => {
-                  modelsApi.request("api/car/models/" + item);
-                  if (values["make"] !== item) {
-                    setFieldValue("model", "");
-                  }
-                }}
-              />
-            </View>
-            <View style={styles.fieldContainer}>
-              <AppText style={styles.fieldTitle}>Model</AppText>
-              <AppFormPicker
-                name="model"
-                placeholder="Please select"
-                items={modelsApi.data}
-                disabled={!values["make"]}
-              />
-            </View>
-            <AppErrorMessage error={error} visible={error} />
-            <SubmitButton title="Next" />
-          </>
-        )}
-      </Formik>
-    );
   };
 
   return (
     <>
       <Background />
-      <ActivityIndicator visible={makesApi.loading || modelsApi.loading} />
+      <ActivityIndicator visible={false} />
       <KeyboardAvoidingView
         behavior={Platform.OS == "ios" ? "padding" : ""}
         keyboardVerticalOffset={Platform.OS == "ios" ? 50 : 0}
         style={styles.keyboardAvoidingView}
       >
+        <ProgressBar route={route} />
+        <AppText style={styles.sectionTitle}>Warranty Detail</AppText>
         <ScrollView>
           <Screen style={styles.screen}>
             <View style={[styles.card, { marginBottom: tabBarHeight }]}>
-              <AppText style={styles.sectionTitle}>Warranty Detail</AppText>
-              <WithRegButton
-                text="With Registration"
-                icon="check-circle"
-                onPress={() => setWithReg(true)}
-                selected={withReg}
-              />
-              <WithRegButton
-                text="Without Registration"
-                icon="close-circle"
-                onPress={async () => {
-                  setWithReg(false);
-                  await makesApi.request();
+              <AppForm
+                initialValues={{
+                  purchase_date: "",
+                  start_date: "",
+                  mot_date: "",
+                  service_date: "",
+                  service_history: "",
+                  registration: registration || "",
+                  vin_number: "",
                 }}
-                selected={!withReg}
+                onSubmit={handleSubmit}
+                validationSchema={validationSchema}
+              >
+                <View style={styles.fieldContainer}>
+                  <AppText style={styles.fieldTitle}>Purchase Date</AppText>
+                  <AppFormDateTimePicker name="purchase_date" />
+                </View>
+                <View style={styles.fieldContainer}>
+                  <AppText style={styles.fieldTitle}>
+                    Warranty Start Date
+                  </AppText>
+                  <AppFormDateTimePicker name="start_date" />
+                </View>
+                <View style={styles.fieldContainer}>
+                  <AppText style={styles.fieldTitle}>MOT Due Date</AppText>
+                  <AppFormDateTimePicker name="mot_date" />
+                </View>
+                <View style={styles.fieldContainer}>
+                  <AppText style={styles.fieldTitle}>Last Service Date</AppText>
+                  <AppFormDateTimePicker name="service_date" />
+                </View>
+                <View style={styles.fieldContainer}>
+                  <AppText style={styles.fieldTitle}>Service History</AppText>
+                  <AppFormPicker
+                    name="service_history"
+                    items={serviceHistoryOptions}
+                  />
+                </View>
+                <View style={styles.fieldContainer}>
+                  <AppText style={styles.fieldTitle}>Registration</AppText>
+                  <RegistrationPlateInput
+                    name="registration"
+                    placeholder="ENTER REGISTRATION"
+                  />
+                </View>
+                <View style={styles.fieldContainer}>
+                  <AppText style={styles.fieldTitle}>VIN Number</AppText>
+                  <AppFormField
+                    name="vin_number"
+                    placeholder="VIN Number"
+                    style={styles.appFormField}
+                  />
+                </View>
+                <SubmitButton title="Next" />
+              </AppForm>
+              <AppButton
+                backgroundColor={null}
+                color={colors.success}
+                title="Back"
+                onPress={() => navigation.goBack()}
               />
-              <View style={styles.formContainer}>
-                {withReg ? <WithRegForm /> : <WithoutRegForm />}
-                <AppButton
-                  backgroundColor={null}
-                  color={colors.success}
-                  title="Back"
-                  onPress={() => navigation.goBack()}
-                />
-              </View>
             </View>
           </Screen>
         </ScrollView>
@@ -213,34 +186,9 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 22,
     fontWeight: "bold",
-    color: colors.secondary,
-    marginBottom: 10,
-  },
-  withRegButton: {
+    color: "white",
+    marginLeft: 20,
     marginVertical: 10,
-    padding: 20,
-    borderRadius: 20,
-    borderColor: colors.mediumGrey,
-    borderWidth: 2,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  withRegButtonSelected: {
-    borderColor: colors.primary,
-    borderWidth: 2,
-    backgroundColor: "white",
-    ...defaultStyles.shadow,
-  },
-  withRegText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: colors.mediumGrey,
-  },
-  withRegTextSelected: {
-    color: colors.darkGrey,
-  },
-  formContainer: {
-    marginTop: 30,
   },
   fieldContainer: {
     marginBottom: 10,
