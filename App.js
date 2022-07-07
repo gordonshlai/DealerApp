a6b7ad45db693516d347d0f290bc9628d2dc4ab5
Gordon Lai <gordon@builtbypixel.com>
2022-07-07 21:35:01 +0100

    expo-app-loading deprecated, replaced module using expo-splash-screen

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
 import AuthContext from "./app/auth/context";
 import AppNavigator from "./app/navigation/AppNavigator";
 import authStorage from "./app/auth/storage";
+import * as SplashScreen from "expo-splash-screen";
 
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import client from "./app/api/client";
-import { StatusBar } from "react-native";
+import { StatusBar, View } from "react-native";
 // import OnboardingScreen from "./app/screens/OnboardingScreen";
 // import AsyncStorage from "@react-native-async-storage/async-storage";
 import { navigationRef } from "./app/navigation/rootNavigation";
@@ -25,8 +25,45 @@ export default function App() {
   const [loadInventoryDetailFlag, setLoadInventoryDetailFlag] = useState(false);
 
   useEffect(() => {
-    // repeatRequest();
-  }, [authToken]);
+    restoreToken();
+  }, []);
+
+  const restoreToken = async () => {
+    try {
+      // Keep the splash screen visible while we fetch resources
+      await SplashScreen.preventAutoHideAsync();
+      const token = await authStorage.getToken();
+      if (token) {
+        const user = await client.get("api/user");
+        if (!user.ok) {
+          setAuthToken(null);
+          authStorage.removeToken();
+          return;
+        }
+        setAuthToken(token);
+      }
+    } catch (error) {
+      console.warn(error);
+    } finally {
+      // Tell the application to render
+      setIsReady(true);
+    }
+  };
+
+  const onLayoutRootView = async () => {
+    if (isReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  useEffect(() => {
+    onLayoutRootView();
+  }, [isReady]);
 
   // useEffect(() => {
   //   firstLaunch();
@@ -45,48 +82,11 @@ export default function App() {
   //   });
   // };
 
-  const repeatRequest = async () => {
-    if (!authToken) return;
-
-    const storageToken = await authStorage.getToken();
-    if (!storageToken) return;
-
-    const user = await client.get("api/user");
-    if (!user.ok) return;
-
-    const result = await client.get("api/inbox/unread");
-    console.log(result.data);
-    setUnread(result.data);
-
-    setTimeout(() => {
-      repeatRequest();
-    }, 30000);
-  };
-
   /**
    * restore the authentication json web token from the secure storage of the application.
    */
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (token) {
-      const user = await client.get("api/user");
-      if (!user.ok) {
-        setAuthToken(null);
-        authStorage.removeToken();
-        return;
-      }
-      setAuthToken(token);
-    }
-  };
 
-  if (!isReady)
-    return (
-      <AppLoading
-        startAsync={restoreToken}
-        onFinish={() => setIsReady(true)}
-        onError={(error) => console.log(error)}
-      />
-    );
+  if (!isReady) return null;
   return (
     <AuthContext.Provider
       value={{
